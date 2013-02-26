f49d829424691854502c56520b065496d06b1e38
mickymiseck <themiseck.rock@gmail.com>
2013-02-26 15:08:07 -0500

    add url for region json response

--- ubigeo/urls.py
+++ ubigeo/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('ubigeo.views',
+    url(r'^region/json/$', 'region', name='ubigeo-region-json'),
     url(r'^provincia/json/$', 'provincia', name='ubigeo-provincia-json'),
     url(r'^distrito/json/$', 'distrito', name='ubigeo-distrito-json'),
 )
